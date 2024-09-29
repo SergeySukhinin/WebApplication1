2c920340c423b0e167e1aa208f5c3e6d1e3cf6e7
Sergey Sukhinin <3723901@gmail.com>
2024-09-29 13:19:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using WebApplication1.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();    //very important to use AddTransient method in order to make the extension method
+                                                        // working and being able to get executed
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+//Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("From Middleware 1\n");
+    await next(context);
+});
+
+//Middleware 2
+//app.UseMiddleware<MyCustomMiddleware>(); - a call of the function without using an extension method
+app.UseMyCustomMiddleware();    //using Extension method
+
+
+//Middleware 3
+app.Run(async (HttpContext context) => {
+    //await context.Response.WriteAsync($"{nameof(MyCustomMiddleware)}");
+    await context.Response.WriteAsync("Middleware 3\n");
+});
+
+
+app.Run();
